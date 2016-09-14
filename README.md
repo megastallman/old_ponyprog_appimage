aecf1871fe99924c91da555cfec4cc474b3870f3
megastallman <megastallman@gmail.com>
2016-09-14 16:38:45 +0300

    Added a pure 64-bit build recipe.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # old_ponyprog_appimage
-Appimage binary built from an old 32-bit Ponyprog2000 version. Runs on 64-bits
-You can also run ponyprog2000 from ponyprog.tar.xz with ponyprog.sh. The same environment is used in Appimage binary
+
+Ponyprog2000 is a nice and simple IC programming software, written by Claudio Lanconelli.
+http://lancos.com/prog.html
+Actually this package has an eventually support and sometimes was difficult to install in certain distros.
+
+Here you can find:
+- An AppImage ponyprog2000 package, built from source(pure 64-bit).
+- A standalone ponyprog2000 binary.
+- A recipe to build everything.
+- Patched sources.
+- An old deprecated AppImage(needs i386-arch support in your distro, not recommended).
+- An old deprecated tarball with old Fedora/Mandriva/Suse/RedHat libs(suitable for 32-bit machines or 64-bit machines with i386-arch enabled, not recommended).
---
+++ ponyprog2000-recipe.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Install build deps if needed
+#sudo apt install build-essential libxmu-dev libxaw7-dev libxt-dev
+
+PROJDIR=$PWD
+
+rm -rf ~/PonyProg2000-2.08c || true
+rm -rf ponyprog2000.AppDir || true
+
+if [ ! -f PonyProg2000-2.08c.tar.gz  ]; then
+	echo "Downloading Ponyprog sources"
+	wget -c http://goo.gl/rcfLqr -O PonyProg2000-2.08c.tar.gz
+fi
+
+# These ugly build scripts should be run from home
+cd ~/
+tar -xf $PROJDIR/PonyProg2000-2.08c.tar.gz
+sed -i '145d' ~/Pony*/v/srcx/Makefile 
+cd ~/PonyProg* 
+./config.sh $HOME 
+make clean 
+make
+
+cd $PROJDIR
+
+cp ~/PonyProg2000-2.08c/bin/ponyprog2000 .
+
+rm -rf ~/PonyProg2000-2.08c || true
+
+mkdir ponyprog2000.AppDir && cd ponyprog2000.AppDir
+cp ../ponyprog2000 .
+cp ../ponyprog2000.png .
+cat >ponyprog2000.desktop <<EOF
+[Desktop Entry]
+Name=ponyprog2000
+Exec=AppRun
+Icon=ponyprog2000
+Categories=Electronics
+Type=Application
+EOF
+
+cat >AppRun <<EOF
+#!/bin/sh
+PAR=\$(dirname "$(readlink -f "$0")")
+echo \$PAR
+exec "\$PAR/ponyprog2000" "\$@"
+EOF
+chmod +x AppRun
+
+cd $PROJDIR
+rm ponyprog2000.AppImage || true
+./AppImageAssistant* ponyprog2000.AppDir ponyprog2000.AppImage
+chmod +x ponyprog2000.AppImage
